4bd7e39c11ed8f91dfd38f29c777ea94fd455f97
thebit52409 <aslamuddin0015@gmail.com>
2022-01-26 08:40:05 -0500

    Updated

--- Project_1/static/JS/javascript.js
+++ Project_1/static/JS/javascript.js
@@ -24,22 +24,40 @@ function generateCat(){
     div.appendChild(image);
 }
 
-//Challenge 3: Rock, Paper, Scissor
+//Challenge 3: Rock, Paper, Scissors
 
 function rpsGame(yourChoice){
     console.log(yourChoice.id);
     var humanChoice, botChoice;
-    //humanChoice = yourChoice.id;
-    //botChoice = ;
+    humanChoice = yourChoice.id;
+    console.log(humanChoice);
+    botChoice = numberToChoice(randToRpsInt);
+    console.log(botChoice);
     results = decideWinner(humanChoice, botChoice);
-    message = finalMessage(results); // { 'message' : "You Won!", 'color' : "green"}
-    rpsFronteEnd(yourChoice.id, botChoice, message);
+   console.log(results);
+    // console.log(results);
+    
+    //message = finalMessage(results); // { 'message' : "You Won!", 'color' : "green"}
+    //rpsFronteEnd(yourChoice.id, botChoice, message);
 }
 
 function randToRpsInt(){
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random()*3);
 }
 
 function numberToChoice(number){
     return ['rock', 'paper', 'scissors'][number];
+}
+
+function decideWinner(yourChoice, computerChoice){
+    var rpsDatabase = {
+        'rock': {'scissors': 1, 'rock' : 0.5, 'paper': 0},
+        'paper': {'rock': 1, 'paper': 0.5, 'scissors':0},
+        'scissors': {'paper': 1, 'scissors': 0.5, 'rock':0}
+    };
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore, computerScore];
 }
