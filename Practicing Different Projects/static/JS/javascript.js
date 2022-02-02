37b0106f6474ff92e3d01304b175c55c22fab465
thebit52409 <aslamuddin0015@gmail.com>
2022-02-01 23:54:36 -0500

    Updated

--- Practicing Different Projects/static/JS/javascript.js
+++ Practicing Different Projects/static/JS/javascript.js
@@ -41,7 +41,7 @@ function rpsGame(yourChoice){
 
     message = finalMessage(results);
     console.log(message);
-    //rpsFrontEnd(yourChoice.id, botChoice, message);
+    rpsFrontEnd(yourChoice.id, botChoice, message);
 }
 
 function randomRpsInt(){
@@ -74,4 +74,30 @@ function finalMessage([yourScore, computerScore]){
     else{
         return {'message': 'You Win!', 'color': 'green'};
     }
+}
+
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage){
+    var imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src
+    }
+
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "' height=150 width=150 style='box-shadow:0px 10px 50px rgba(37, 50, 233, 1)'>";
+    messageDiv.innerHTML = "<h1 style='color: "+finalMessage['color'] + "; font-size:60px; padding:30px;'>"+ finalMessage['message'] + "</h1>";
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "' height=150 width=150 style='box-shadow:0px 10px 50px rgba(243, 38, 24,1)'>";
+
+
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+    document.getElementById('flex-box-rps-div').appendChild(botDiv);
 }
